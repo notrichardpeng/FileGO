828f66d976d5d60e14e64e15e520c9da674fdb7c
notrichardpeng <58278719+notrichardpeng@users.noreply.github.com>
2020-09-15 22:54:08 -0400

    Added moving any file from Downloads to Another folder

--- foldersort.py
+++ foldersort.py
@@ -1,12 +1,19 @@
 import time
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 track_folder = "C:\\Users\\notri\\Downloads"
+target_folder = "C:\\Users\\notri\\Desktop\\School"
 
 class Handler(FileSystemEventHandler):
+	
 	def on_modified(self, event):
-		print("wtf")
+		for filename in os.listdir(track_folder):		
+			print("?")	
+			curr_dir = track_folder + "\\" + filename	
+			new_dir = target_folder + "\\" + filename
+			os.rename(curr_dir, new_dir)
 
 if __name__ == "__main__":
 	observer = Observer()
